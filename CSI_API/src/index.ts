d3593fe59fbc1826c634899c8acad600b4121bec
Thomas Watson <twatson071@gmail.com>
2025-06-04 09:32:41 -0500

    Add mock server endpoint and alert validation

--- CSI_API/src/index.ts
+++ CSI_API/src/index.ts
@@ -4,6 +4,7 @@ import "./db"; // ensure .env loads & db.ts runs
 import pdu from "./routes/PDUroutes/pduRoutes";
 import sites from "./routes/sites/sitesRoutes";
 import devices from "./routes/devices/deviceRoutes";
+import mock from "./routes/mock/mockRoutes";
 
 const app = new Hono();
 app.use("*", cors({ origin: "*" })); // Enable CORS for all routes
@@ -11,4 +12,5 @@ app.use("*", cors({ origin: "*" })); // Enable CORS for all routes
 app.route("/pdu", pdu);
 app.route("/sites", sites);
 app.route("/devices", devices);
+app.route("/mock", mock);
 export default app;
